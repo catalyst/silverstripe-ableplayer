bcfe1abaeba42b2ffe7915a8c8b259fa17f9771f
Elliot Sawyer <elliotsawyer@catalyst.net.nz>
2020-08-19 14:52:12 +1200

    initial commit

---
+++ src/Shortcodes/AccessibleVideoShortcodeProvider.php
@@ -0,0 +1,36 @@
+<?php
+namespace Catalyst\AblePlayer;
+
+use SilverStripe\View\Parsers\ShortcodeHandler;
+
+class AccessibleVideoShortcodeProvider implements ShortcodeHandler
+{
+
+    /**
+     * Gets the list of shortcodes provided by this handler
+     *
+     * @return mixed
+     */
+    public static function get_shortcodes()
+    {
+        return 'video_link';
+    }
+
+    /**
+     * Replace "[video_link id=n]" shortcode w3c markup.
+     *
+     * @param array $arguments Arguments passed to the parser
+     * @param string $content Raw shortcode
+     * @param ShortcodeParser $parser Parser
+     * @param string $shortcode Name of shortcode used to register this handler
+     * @param array $extra Extra arguments
+     *
+     * @return string Result of the handled shortcode
+     */
+    public static function handle_shortcode($arguments, $content, $parser, $shortcode, $extra = [])
+    {
+
+    }
+
+
+}
